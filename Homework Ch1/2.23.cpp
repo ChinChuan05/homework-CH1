cd438333812ae04c0fc340bbea62b6af43048640
brian <0103.brian@gmail.com>
2022-09-30 14:57:44 +0800

    secend

---
+++ Homework Ch1/2.23.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int num1, num2, num3, largest;
+
+int main() {
+	printf("Please input three number¡G");
+	scanf_s("%d%d%d", &num1, &num2, &num3);
+	
+	largest = num1;
+	if (num2 > largest) {
+		largest = num2;
+	}
+	if (num3 > largest) {
+		largest = num3;
+	}
+	printf("The largest num is %d", largest);
+}
