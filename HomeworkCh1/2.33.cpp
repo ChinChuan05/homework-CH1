a0c4f38a74bf7374b0c5e4b0c9fa921502611400
chuan <0103.brian@gmail.com>
2022-10-03 20:51:51 +0800

    finish homework CH1

--- HomeworkCh1/2.33.cpp
+++ HomeworkCh1/2.33.cpp
@@ -1,9 +1,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-int dis, gas_fee, gas_dis, park, toll;
+int dis, gas_fee, gas_dis, park, toll, total_cost;
 
 int main(){
     printf("Please input the data (1)Total distant (2)gaslin per liter (3)distance per liter (4)parking fee (5)Tolls: ");
-    scanf("%d%d%d%d%d", dis, gas_fee, gas_dis, park, toll);
+    scanf("%d%d%d%d%d", &dis, &gas_fee, &gas_dis, &park, &toll);
+
+    total_cost = (dis/gas_dis) * gas_fee + park + toll;
+
+    printf("Drive car total cost %d dollar\n", total_cost);
 }
